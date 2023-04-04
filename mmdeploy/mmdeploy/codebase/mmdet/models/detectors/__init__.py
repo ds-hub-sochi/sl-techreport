ee99092c136109cce9f90430fc4228010aae594f
Leonid Verkhovtsev <leoromanovich@gmail.com>
2023-04-04 22:40:00 +0300

    reupload mmdeploy wrt to used link in readme

--- mmdeploy/mmdeploy/codebase/mmdet/models/detectors/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from . import single_stage, single_stage_instance_seg, two_stage
-
-__all__ = ['single_stage', 'single_stage_instance_seg', 'two_stage']
--- mmdeploy/mmdeploy/codebase/mmdet3d/deploy/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .voxel_detection import MMDetection3d, VoxelDetection
-from .voxel_detection_model import VoxelDetectionModel
-
-__all__ = ['MMDetection3d', 'VoxelDetection', 'VoxelDetectionModel']
